6124e44e231b036e170873b0cbdc3f97ff20b19d
mega-gigaMynetest <qlepine04@gmail.com>
2020-12-07 18:34:31 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
+local random = math.random
+local function grow_rythium_sapling(...)
+	return default.grow_rythium_sapling(...)
+end
+
+
 minetest.register_craftitem("rythium:diamond_powder", {
 	description = ("Diamond powder"),
 	inventory_image = "diamond_powder.png",
@@ -37,3 +43,242 @@ minetest.register_craft( {
                 {"", "rythium:diamond_powder", ""},
         },
 })
+
+minetest.register_node("rythium:sapling", {
+	description = ("rythium sapling"),
+	drawtype = "plantlike",
+	tiles = {"azerty.png"},
+	inventory_image = "default_sapling.png",
+	wield_image = "default_sapling.png",
+	paramtype = "light",
+	sunlight_propagates = true,
+	walkable = false,
+	on_timer = grow_rythium_sapling,
+	selection_box = {
+		type = "fixed",
+		fixed = {-4 / 16, -0.5, -4 / 16, 4 / 16, 7 / 16, 4 / 16}
+	},
+	groups = {snappy = 2, dig_immediate = 3, flammable = 2,
+		attached_node = 1, sapling = 1},
+	sounds = default.node_sound_leaves_defaults(),
+  on_construct = function(pos)
+    minetest.get_node_timer(pos):start(math.random(10, 20))
+  end,
+
+  on_place = function(itemstack, placer, pointed_thing)
+    itemstack = default.sapling_on_place(itemstack, placer, pointed_thing,
+      "rythium:sapling",
+      -- minp, maxp to be checked, relative to sapling pos
+      -- minp_relative.y = 1 because sapling pos has been checked
+      {x = -3, y = 1, z = -3},
+      {x = 3, y = 6, z = 3},
+      -- maximum interval of interior volume check
+      4)
+
+    return itemstack
+  end,
+})
+
+
+
+
+local S = default.get_translator
+
+
+function default.can_grow(pos)
+	local node_under = minetest.get_node_or_nil({x = pos.x, y = pos.y - 1, z = pos.z})
+	if not node_under then
+		return false
+	end
+	local name_under = node_under.name
+	if name_under ~= "rythium:mineral_dirt" then
+		return false
+	end
+	local light_level = minetest.get_node_light(pos)
+	if not light_level or light_level < 13 then
+		return false
+	end
+	return true
+end
+
+
+local function is_snow_nearby(pos)
+	return minetest.find_node_near(pos, 1, {"group:snowy"})
+end
+
+
+function default.grow_rythium_sapling(pos)
+	if not default.can_grow(pos) then
+
+		minetest.get_node_timer(pos):start(10)
+		return
+	end
+
+	local mg_name = minetest.get_mapgen_setting("mg_name")
+	local node = minetest.get_node(pos)
+	if node.name == "rythium:sapling" then
+		minetest.log("action", "A rythium sapling grows into a rythium tree at "..
+			minetest.pos_to_string(pos))
+    default.grow_new_apple_tree(pos)
+  end
+end
+
+--
+-- Tree generation
+--
+
+-- Apple tree and jungle tree trunk and leaves function
+
+local function add_trunk_and_leaves_rythium(data, a, pos, tree_cid, leaves_cid,
+		height, size, iters, is_apple_tree)
+	local x, y, z = pos.x, pos.y, pos.z
+	local c_air = minetest.get_content_id("air")
+	local c_ignore = minetest.get_content_id("ignore")
+	local c_apple = minetest.get_content_id("default:apple")
+
+	-- Trunk
+	data[a:index(x, y, z)] = tree_cid -- Force-place lowest trunk node to replace sapling
+	for yy = y + 1, y + height - 1 do
+		local vi = a:index(x, yy, z)
+		local node_id = data[vi]
+		if node_id == c_air or node_id == c_ignore or node_id == leaves_cid then
+			data[vi] = tree_cid
+		end
+	end
+
+	-- Force leaves near the trunk
+	for z_dist = -1, 1 do
+	for y_dist = -size, 1 do
+		local vi = a:index(x - 1, y + height + y_dist, z + z_dist)
+		for x_dist = -1, 1 do
+			if data[vi] == c_air or data[vi] == c_ignore then
+				if is_apple_tree and random(1, 8) == 1 then
+					data[vi] = c_apple
+				else
+					data[vi] = leaves_cid
+				end
+			end
+			vi = vi + 1
+		end
+	end
+	end
+end
+
+-- Apple tree
+
+function default.grow_tree(pos, is_apple_tree, bad)
+	--[[
+		NOTE: Tree-placing code is currently duplicated in the engine
+		and in games that have saplings; both are deprecated but not
+		replaced yet
+	--]]
+	if bad then
+		error("Deprecated use of default.grow_tree")
+	end
+
+	local x, y, z = pos.x, pos.y, pos.z
+	local height = random(6, 8)
+	local c_rythium_tree = minetest.get_content_id("default:tree")
+	local c_rythium_leaves = minetest.get_content_id("default:leaves")
+
+	local vm = minetest.get_voxel_manip()
+	local minp, maxp = vm:read_from_map(
+		{x = x - 2, y = y, z = z - 2},
+		{x = x + 2, y = y + height + 1, z = z + 2}
+	)
+	local a = VoxelArea:new({MinEdge = minp, MaxEdge = maxp})
+	local data = vm:get_data()
+
+	add_trunk_and_leaves_rythium(data, a, pos, c_rythium_tree, c_rythium_leaves, height, 2, 8, false)
+
+	vm:set_data(data)
+	vm:write_to_map()
+	vm:update_map()
+end
+
+function default.sapling_on_place(itemstack, placer, pointed_thing,
+		sapling_name, minp_relative, maxp_relative, interval)
+	-- Position of sapling
+	local pos = pointed_thing.under
+	local node = minetest.get_node_or_nil(pos)
+	local pdef = node and minetest.registered_nodes[node.name]
+
+	if pdef and pdef.on_rightclick and
+			not (placer and placer:is_player() and
+			placer:get_player_control().sneak) then
+		return pdef.on_rightclick(pos, node, placer, itemstack, pointed_thing)
+	end
+
+	if not pdef or not pdef.buildable_to then
+		pos = pointed_thing.above
+		node = minetest.get_node_or_nil(pos)
+		pdef = node and minetest.registered_nodes[node.name]
+		if not pdef or not pdef.buildable_to then
+			return itemstack
+		end
+	end
+
+	local player_name = placer and placer:get_player_name() or ""
+	-- Check sapling position for protection
+	if minetest.is_protected(pos, player_name) then
+		minetest.record_protection_violation(pos, player_name)
+		return itemstack
+	end
+	-- Check tree volume for protection
+	if minetest.is_area_protected(
+			vector.add(pos, minp_relative),
+			vector.add(pos, maxp_relative),
+			player_name,
+			interval) then
+		minetest.record_protection_violation(pos, player_name)
+		-- Print extra information to explain
+--		minetest.chat_send_player(player_name,
+--			itemstack:get_definition().description .. " will intersect protection " ..
+--			"on growth")
+		minetest.chat_send_player(player_name,
+		    S("@1 will intersect protection on growth.",
+			itemstack:get_definition().description))
+		return itemstack
+	end
+
+	minetest.log("action", player_name .. " places node "
+			.. sapling_name .. " at " .. minetest.pos_to_string(pos))
+
+	local take_item = not (creative and creative.is_enabled_for
+		and creative.is_enabled_for(player_name))
+	local newnode = {name = sapling_name}
+	local ndef = minetest.registered_nodes[sapling_name]
+	minetest.set_node(pos, newnode)
+
+	-- Run callback
+	if ndef and ndef.after_place_node then
+		-- Deepcopy place_to and pointed_thing because callback can modify it
+		if ndef.after_place_node(table.copy(pos), placer,
+				itemstack, table.copy(pointed_thing)) then
+			take_item = false
+		end
+	end
+
+	-- Run script hook
+	for _, callback in ipairs(minetest.registered_on_placenodes) do
+		-- Deepcopy pos, node and pointed_thing because callback can modify them
+		if callback(table.copy(pos), table.copy(newnode),
+				placer, table.copy(node or {}),
+				itemstack, table.copy(pointed_thing)) then
+			take_item = false
+		end
+	end
+
+	if take_item then
+		itemstack:take_item()
+	end
+
+	return itemstack
+end
+
+function default.grow_new_apple_tree(pos)
+	local path = minetest.get_modpath("default") ..
+		"/schematics/apple_tree_from_sapling.mts"
+	minetest.place_schematic({x = pos.x - 3, y = pos.y - 1, z = pos.z - 3},
+		path, "random", nil, false)
+end
