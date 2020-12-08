a98dbe881399353adf017aa9b8fe754f9c330e78
mega-gigaMynetest <qlepine04@gmail.com>
2020-12-08 15:49:31 +0100

    Update nodes.lua

---
+++ nodes.lua
@@ -0,0 +1,100 @@
+local function grow_rythium_sapling(...)
+	return default.grow_rythium_sapling(...)
+end
+local S = default.get_translator
+local random = math.random
+
+minetest.register_craftitem("rythium:diamond_powder", {
+	description = ("Diamond powder"),
+	inventory_image = "diamond_powder.png",
+})
+
+minetest.register_craftitem("rythium:mithril_powder", {
+	description = ("Mithril_powder"),
+	inventory_image = "mithril_powder.png",
+})
+minetest.register_node("rythium:mineral_dirt", {
+  description = ("Mineral fertilized dirt"),
+  tiles = {"mineral_dirt.png"},
+  groups = {crumbly = 3, soil = 1},
+  sounds = default.node_sound_dirt_defaults({
+		footstep = {name = "default_grass_footstep", gain = 0.25},
+	}),
+})
+
+minetest.register_craftitem("rythium:rythium_ingot", {
+	description = ("Rythium ingot"),
+	inventory_image = "rythium_ingot.png"
+}
+)
+
+minetest.register_node("rythium:rythium_nugget", {
+	description = ("rythium nugget"),
+	drawtype = "plantlike",
+	tiles = {"rythium_nugget.png"},
+	inventory_image = "rythium_nugget.png",
+	paramtype = "light",
+	sunlight_propagates = true,
+	walkable = false,
+	is_ground_content = false,
+	selection_box = {
+		type = "fixed",
+		fixed = {-3 / 16, -7 / 16, -3 / 16, 3 / 16, 4 / 16, 3 / 16}
+	},
+	groups = {leafdecay = 3, leafdecay_drop = 1, cracky = 1},
+	sounds = default.node_sound_leaves_defaults(),
+})
+
+minetest.register_node("rythium:leaves", {
+	description = ("Rythium leaves"),
+	drawtype = "allfaces_optional",
+	waving = 1,
+	tiles = {"rythium_leaves.png"},
+	paramtype = "light",
+	is_ground_content = false,
+	groups = {snappy = 3, leafdecay = 3, flammable = 2, leaves = 1},
+	sounds = default.node_sound_leaves_defaults(),
+})
+
+minetest.register_craft( {
+        output = "rythium:rythium_ingot",
+        recipe = {
+                {"rythium:rythium_nugget", "rythium:rythium_nugget", "rythium:rythium_nugget" },
+                { "rythium:rythium_nugget", "rythium:rythium_nugget", "rythium:rythium_nugget" },
+                {"rythium:rythium_nugget", "rythium:rythium_nugget", "rythium:rythium_nugget"},
+        },
+})
+
+minetest.register_node("rythium:sapling", {
+	description = ("rythium sapling"),
+	drawtype = "plantlike",
+	tiles = {"rythium_sapling.png"},
+	inventory_image = "rythium_sapling.png",
+	wield_image = "rythium_sapling.png",
+	paramtype = "light",
+	sunlight_propagates = true,
+	walkable = false,
+	on_timer = grow_rythium_sapling,
+	selection_box = {
+		type = "fixed",
+		fixed = {-4 / 16, -0.5, -4 / 16, 4 / 16, 7 / 16, 4 / 16}
+	},
+	groups = {snappy = 2, dig_immediate = 3, flammable = 2,
+		attached_node = 1, sapling = 1},
+	sounds = default.node_sound_leaves_defaults(),
+  on_construct = function(pos)
+    minetest.get_node_timer(pos):start(math.random(10, 20))
+  end,
+
+  on_place = function(itemstack, placer, pointed_thing)
+    itemstack = default.sapling_on_place(itemstack, placer, pointed_thing,
+      "rythium:sapling",
+      -- minp, maxp to be checked, relative to sapling pos
+      -- minp_relative.y = 1 because sapling pos has been checked
+      {x = -3, y = 1, z = -3},
+      {x = 3, y = 6, z = 3},
+      4)
+
+    return itemstack
+  end,
+})
