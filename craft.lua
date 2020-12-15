f313acf2a250cdc563fc444de993cd1d576c6129
Grossam <lejimi1972@free.fr>
2020-12-15 11:00:06 +0100

    Healing wand drafted

--- craft.lua
+++ craft.lua
@@ -1,8 +1,8 @@
-local function grow_rythium_sapling(...)
-	return default.grow_rythium_sapling(...)
-end
-local S = default.get_translator
-local random = math.random
+--local function grow_rythium_sapling(...)
+--	return default.grow_rythium_sapling(...)
+--end
+--local S = default.get_translator
+--local random = math.random
 
 minetest.register_craft({
 	type = "cooking",
@@ -11,7 +11,7 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
-  type = "cooking",
+	type = "cooking",
 	output = "rythium:diamond_powder",
 	recipe = "default:diamond",
 })
@@ -19,19 +19,28 @@ minetest.register_craft({
 
 
 minetest.register_craft( {
-        output = "rythium:sapling",
-        recipe = {
-                {"rythium:diamond_powder", "default:mese_crystal", "rythium:diamond_powder" },
-                {"rythium:mithril_powder", "default:mese_crystal", "rythium:mithril_powder" },
-                {"", "default:stick", ""},
-        },
+	output = "rythium:sapling",
+	recipe = {
+		{"rythium:diamond_powder", "default:mese_crystal", "rythium:diamond_powder" },
+		{"rythium:mithril_powder", "default:mese_crystal", "rythium:mithril_powder" },
+		{"", "default:stick", ""},
+	},
 })
 
 minetest.register_craft( {
-        output = "rythium:mineral_dirt",
-        recipe = {
-                {"", "rythium:diamond_powder", "" },
-                { "rythium:mithril_powder", "default:dirt", "rythium:mithril_powder" },
-                {"", "rythium:diamond_powder", ""},
-        },
+	output = "rythium:mineral_dirt",
+	recipe = {
+		{"", "rythium:diamond_powder", "" },
+		{ "rythium:mithril_powder", "default:dirt", "rythium:mithril_powder" },
+		{"", "rythium:diamond_powder", ""},
+	},
+})
+
+minetest.register_craft( {
+	output = "rythium:healing_wand",
+	recipe = {
+		{"default:apple", "rythium:rythium_ingot", "default:apple"},
+		{"default:apple", "rythium:rythium_ingot", "default:apple"},
+		{"", "default:stick", ""},
+	},
 })
--- tools.lua
+++ tools.lua
@@ -23,8 +23,15 @@ minetest.register_tool("rythium:healing_wand", {
 	description = S("Healing wand"),
 	inventory_image = "rythium_healing_wand.png",
 	on_use = function(itemstack, user, pointed_thing)
-		local player = user
 		minetest.sound_play("rythium_healing")
-		player:set_hp(20)
+		-- pointed_thing is a table and this table contains a ref variable,
+		-- i.e. another table which represents the pointed object itself.
+		-- difference is tested with ~= not with != as in other languages
+		if (pointed_thing.ref~=nil and pointed_thing.ref:is_player())
+		then
+			pointed_thing.ref:set_hp(20)
+		else
+			user:set_hp(20)
+		end
 	end,
 })
