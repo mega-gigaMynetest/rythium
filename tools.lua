4acb02369b8032aebc0a46c8b3efe3307c09f311
sys4 <admin@sys4.fr>
2020-12-16 22:35:50 +0100

    Add a 3x3 digging Rythium Pickaxe

--- tools.lua
+++ tools.lua
@@ -26,3 +26,136 @@ minetest.register_tool("rythium:healing_wand", {
 		end
 	end,
 })
+
+--
+-- Rythium Pickaxe
+--
+
+local function dig_it(pos, player)
+	if minetest.is_protected(pos, player:get_player_name()) then
+		minetest.record_protection_violation(pos, player:get_player_name())
+      return
+   end
+   local node = minetest.get_node(pos)
+   if node.name == "air" or node.name == "ignore" then return end
+   if node.name == "default:lava_source" then return end
+   if node.name == "default:lava_flowing" then return end
+	minetest.remove_node(pos)
+end
+
+local function dig_dir(player)
+	local dir=player:get_look_dir()
+	if math.abs(dir.x)>math.abs(dir.z) then
+		if dir.x>0 then return 0 end
+		return 1
+	end
+	if dir.z>0 then return 2 end
+	return 3
+end
+
+local function dig_it1 (pos,player)
+	pos.y=pos.y+1
+	dig_it (pos,player)
+	pos.z=pos.z+1
+	dig_it (pos,player)
+	pos.z=pos.z-2
+	dig_it (pos,player)
+	pos.z=pos.z+1
+	pos.y=pos.y-1
+	dig_it (pos,player)
+	pos.z=pos.z+1
+	dig_it (pos,player)
+	pos.z=pos.z-2
+	dig_it (pos,player)
+	pos.z=pos.z+1
+	pos.y=pos.y-1
+	dig_it (pos,player)
+	pos.z=pos.z+1
+	dig_it (pos,player)
+	pos.z=pos.z-2
+	dig_it (pos,player)
+end
+
+local function dig_it2 (pos,player)
+	pos.y=pos.y+1
+	dig_it (pos,player)
+	pos.x=pos.x+1
+	dig_it (pos,player)
+	pos.x=pos.x-2
+	dig_it (pos,player)
+	pos.x=pos.x+1
+	pos.y=pos.y-1
+	dig_it (pos,player)
+	pos.x=pos.x+1
+	dig_it (pos,player)
+	pos.x=pos.x-2
+	dig_it (pos,player)
+	pos.x=pos.x+1
+	pos.y=pos.y-1
+	dig_it (pos,player)
+	pos.x=pos.x+1
+	dig_it (pos,player)
+	pos.x=pos.x-2
+	dig_it (pos,player)
+end
+
+local function dig_it3 (pos,player)
+	dig_it (pos,player)
+	pos.x=pos.x+1
+	dig_it (pos,player)
+	pos.x=pos.x-2
+	dig_it (pos,player)
+	pos.x=pos.x+1
+	pos.z=pos.z+1
+	dig_it (pos,player)
+	pos.x=pos.x+1
+	dig_it (pos,player)
+	pos.x=pos.x-2
+	dig_it (pos,player)
+	pos.x=pos.x+1
+	pos.z=pos.z-2
+	dig_it (pos,player)
+	pos.x=pos.x+1
+	dig_it (pos,player)
+	pos.x=pos.x-2
+	dig_it (pos,player)
+end
+
+local function dig_it_dir(pos, player)
+	local dir = player:get_look_dir()
+	if math.abs(dir.y)<0.5 then
+		dir = dig_dir(player)
+		if dir == 0 or dir == 1 then -- x
+			dig_it1(pos, player)
+		end
+		if dir == 2 or dir == 3 then -- z
+			dig_it2(pos, player)
+		end
+	else
+		dig_it3(pos, player)
+	end
+end
+
+minetest.register_tool("rythium:huge_pick", {
+	description = ("3*3 Pick"),
+	inventory_image = "huge_pick.png",
+	tool_capabilities = {
+		full_punch_interval = 0.9,
+		max_drop_level=3,
+		groupcaps={
+			cracky = {times={[1]=2.0, [2]=1.0, [3]=0.50}, uses=40, maxlevel=3},
+		},
+		damage_groups = {fleshy=4},
+	},
+	sound = {breaks = "default_tool_breaks"},
+	groups = {pickaxe = 1}
+})
+
+minetest.register_on_dignode(
+	function(pos, oldnode, digger)
+		if not digger:is_player() then return end
+		if digger:get_wielded_item():get_name() == "rythium:huge_pick" then
+			dig_it_dir(pos, digger)
+		end
+	end
+)
