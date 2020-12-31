mega-gigaMynetest/rythium

commit 1f5177fea326f77212b25b425b154e2c16b9b4c7

Code cleanup

tree.lua
@@ -44,31 +44,8 @@ local function can_grow(pos)
 	return true
 end
 
-function rythium.grow_rythium_sapling(pos)
-	if not can_grow(pos) then
-		minetest.get_node_timer(pos):start(10)
-		return
-	end
-	local mg_name = minetest.get_mapgen_setting("mg_name")
-	local node = minetest.get_node(pos)
-	if node.name == "rythium:sapling" then
-		minetest.log("action", "A rythium sapling grows into a rythium tree at "..
-			minetest.pos_to_string(pos))
-		grow_rythium_tree(pos, 1)
-		minetest.set_node({x = pos.x, y = pos.y - 1, z = pos.z}, {name = "default:dirt"})
-		minetest.set_node({x = pos.x, y = pos.y - 1, z = pos.z + 1}, {name = "default:dirt"})
-		minetest.set_node({x = pos.x, y = pos.y - 1, z = pos.z - 1}, {name = "default:dirt"})
-		minetest.set_node({x = pos.x - 1, y = pos.y - 1, z = pos.z}, {name = "default:dirt"})
-		minetest.set_node({x = pos.x + 1, y = pos.y - 1, z = pos.z}, {name = "default:dirt"})
-		minetest.set_node({x = pos.x - 1, y = pos.y - 1, z = pos.z + 1}, {name = "default:dirt"})
-		minetest.set_node({x = pos.x + 1, y = pos.y - 1, z = pos.z + 1}, {name = "default:dirt"})
-		minetest.set_node({x = pos.x - 1, y = pos.y - 1, z = pos.z- 1}, {name = "default:dirt"})
-		minetest.set_node({x = pos.x + 1, y = pos.y - 1, z = pos.z - 1}, {name = "default:dirt"})
-	end
-end
-
 local function add_trunk_and_leaves_rythium(data, a, pos, tree_cid, leaves_cid,
-		height, size, iters, is_apple_tree)
+		height, size, iters)
 	local x, y, z = pos.x, pos.y, pos.z
 	local c_air = minetest.get_content_id("air")
 	local c_ignore = minetest.get_content_id("ignore")
@@ -88,10 +65,8 @@ local function add_trunk_and_leaves_rythium(data, a, pos, tree_cid, leaves_cid,
 		local vi = a:index(x - 1, y + height + y_dist, z + z_dist)
 		for x_dist = -1, 2 do
 			if data[vi] == c_air or data[vi] == c_ignore then
-				if is_apple_tree and random(1, 50) == 1 then
+				if random(1, 50) == 1 then
 					data[vi] = c_apple
-				else
-					data[vi] = leaves_cid
 				end
 			end
 			vi = vi + 1
@@ -108,10 +83,8 @@ local function add_trunk_and_leaves_rythium(data, a, pos, tree_cid, leaves_cid,
 		for zi = 0, 1 do
 			local vi = a:index(clust_x + xi, clust_y + yi, clust_z + zi)
 			if data[vi] == c_air or data[vi] == c_ignore then
-				if is_apple_tree and random(1, 25) == 1 then
+				if random(1, 25) == 1 then
 					data[vi] = c_apple
-				else
-					data[vi] = leaves_cid
 				end
 			end
 		end
@@ -120,7 +93,7 @@ local function add_trunk_and_leaves_rythium(data, a, pos, tree_cid, leaves_cid,
 	end
 end
 
-function grow_rythium_tree(pos, is_apple_tree)
+local function grow_rythium_tree(pos)
 	local x, y, z = pos.x, pos.y, pos.z
 	local height = random(5, 6)
 	local c_rythium_tree = minetest.get_content_id("default:tree")
@@ -132,8 +105,30 @@ function grow_rythium_tree(pos, is_apple_tree)
 	)
 	local a = VoxelArea:new({MinEdge = minp, MaxEdge = maxp})
 	local data = vm:get_data()
-	add_trunk_and_leaves_rythium(data, a, pos, c_rythium_tree, c_rythium_leaves, height, 2, 8, is_apple_tree)
+	add_trunk_and_leaves_rythium(data, a, pos, c_rythium_tree, c_rythium_leaves, height, 2, 8)
 	vm:set_data(data)
 	vm:write_to_map()
 	vm:update_map()
+end
+
+function rythium.grow_rythium_sapling(pos)
+	if not can_grow(pos) then
+		minetest.get_node_timer(pos):start(10)
+		return
+	end
+	local node = minetest.get_node(pos)
+	if node.name == "rythium:sapling" then
+		minetest.log("action", "A rythium sapling grows into a rythium tree at "..
+			minetest.pos_to_string(pos))
+		grow_rythium_tree(pos)
+		minetest.set_node({x = pos.x, y = pos.y - 1, z = pos.z}, {name = "default:dirt"})
+		minetest.set_node({x = pos.x, y = pos.y - 1, z = pos.z + 1}, {name = "default:dirt"})
+		minetest.set_node({x = pos.x, y = pos.y - 1, z = pos.z - 1}, {name = "default:dirt"})
+		minetest.set_node({x = pos.x - 1, y = pos.y - 1, z = pos.z}, {name = "default:dirt"})
+		minetest.set_node({x = pos.x + 1, y = pos.y - 1, z = pos.z}, {name = "default:dirt"})
+		minetest.set_node({x = pos.x - 1, y = pos.y - 1, z = pos.z + 1}, {name = "default:dirt"})
+		minetest.set_node({x = pos.x + 1, y = pos.y - 1, z = pos.z + 1}, {name = "default:dirt"})
+		minetest.set_node({x = pos.x - 1, y = pos.y - 1, z = pos.z- 1}, {name = "default:dirt"})
+		minetest.set_node({x = pos.x + 1, y = pos.y - 1, z = pos.z - 1}, {name = "default:dirt"})
+	end
 end

craft.lua
@@ -1,7 +1,5 @@
 -- mithril powder
-if minetest.get_modpath("technic") then
-	minetest.register_alias("rythium:mithril_powder","technic:mithril_dust")
-else
+if not minetest.get_modpath("technic") then
 	minetest.register_craft({
 		type = "cooking",
 		output = "rythium:mithril_powder",
@@ -20,7 +18,6 @@ else
 	})
 end
 
--- rythium sapling
 minetest.register_craft({
 	output = "rythium:sapling",
 	recipe = {

init.lua
@@ -1,6 +1,3 @@
--- support for MT game translation.
-local S = minetest.get_translator("rythium")
-
 rythium = {}
 
 dofile(minetest.get_modpath("rythium").."/tree.lua")

nodes.lua
@@ -1,4 +1,4 @@
--- support for MT game translation.
+-- support for translation.
 local S = minetest.get_translator("rythium")
 
 minetest.register_craftitem("rythium:diamond_powder", {
@@ -7,7 +7,9 @@ minetest.register_craftitem("rythium:diamond_powder", {
 })
 
 -- mithril powder
-	if not minetest.get_modpath("technic") then
+if minetest.get_modpath("technic") then
+	minetest.register_alias("rythium:mithril_powder","technic:mithril_dust")
+else
 	minetest.register_craftitem("rythium:mithril_powder", {
 		description = S("Mithril powder"),
 		inventory_image = "mithril_powder.png",

tools.lua
@@ -1,4 +1,4 @@
--- support for MT game translation.
+-- support for translation.
 local S = minetest.get_translator("rythium")
 
 --
@@ -18,7 +18,6 @@ minetest.register_tool("rythium:healing_wand", {
 		minetest.sound_play("rythium_healing", {gain = 0.5})
 		-- pointed_thing is a table and this table contains a ref variable,
 		-- i.e. another table which represents the pointed object itself.
-		-- difference is tested with ~= not with != as in other languages
 		if pointed_thing.ref and pointed_thing.ref:is_player() then
 			pointed_thing.ref:set_hp(20)
 		else
@@ -120,86 +119,85 @@ minetest.register_on_dignode(
 
 -- Night vision headlamp
 
-local rythium_light={users={},timer=0}
+local rythium_light_users = {}
 
 minetest.register_on_leaveplayer(function(player)
- local name=player:get_player_name()
- if rythium_light.users[name]~=nil then rythium_light.users[name]=nil end
+	local name = player:get_player_name()
+	rythium_light_users[name] = nil
 end)
 
 minetest.register_node("rythium:light", {
- description = "light source",
- light_source = 12,
- paramtype = "light",
- walkable=false,
- drawtype = "airlike",
- pointable=false,
- buildable_to=true,
- sunlight_propagates = true,
- groups = {not_in_creative_inventory=1},
- on_construct=function(pos)
-   minetest.get_node_timer(pos):start(2)
- end,
- on_timer = function (pos, elapsed)
-   minetest.set_node(pos, {name="air"})
- end,
+	description = "light source",
+	light_source = 12,
+	paramtype = "light",
+	walkable = false,
+	drawtype = "airlike",
+	pointable = false,
+	buildable_to = true,
+	sunlight_propagates = true,
+	groups = {not_in_creative_inventory = 1},
+	on_construct = function(pos)
+		minetest.get_node_timer(pos):start(2)
+	end,
+	on_timer = function(pos, elapsed)
+		minetest.set_node(pos, {name="air"})
+	end,
 })
 
 
-minetest.register_tool("rythium:headlamp_controler", {
-	description = S("Headlamp Controler"),
-	inventory_image = "rythium_headlamp_controler.png",
+minetest.register_tool("rythium:headlamp_controller", {
+	description = S("Headlamp Controller"),
+	inventory_image = "rythium_headlamp_controller.png",
 	on_use = function(itemstack, user, pointed_thing)
 		local _, inv = armor:get_valid_player(user)
-		local list = inv:get_list("armor")
-		for _, v in ipairs(list) do
-			if v:get_name() == "rythium:headlamp" then
-				rythium_light.users[user:get_player_name()]={player=user,inside=0}
-				break
-			end
+		if inv:contains_item("armor", "rythium:headlamp") then
+			rythium_light_users[user:get_player_name()] = {player = user, inside = 0}
 		end
 	end,
 	on_place = function(itemstack, user, pointed_thing)
-		rythium_light.users[user:get_player_name()] = nil
+		rythium_light_users[user:get_player_name()] = nil
 	end,
 	on_secondary_use = function(itemstack, user, pointed_thing)
-		rythium_light.users[user:get_player_name()] = nil
+		rythium_light_users[user:get_player_name()] = nil
 	end,
 	sound = {breaks = "default_tool_breaks"},
 })
 
 armor:register_armor("rythium:headlamp", {
-		description = ("Headlamp"),
-		inventory_image = "rythium_headlamp.png",
-		groups = {armor_head=1, armor_heal=0, armor_use=10},
-		armor_groups = {fleshy=10},
-		damage_groups = {cracky=1, snappy=1, level=1},
-    on_unequip = function(player, index, stack)
-      rythium_light.users[player:get_player_name()]=nil
-    end,
+	description = S("Headlamp"),
+	inventory_image = "rythium_headlamp.png",
+	groups = {armor_head=1, armor_heal=0, armor_use=10},
+	armor_groups = {fleshy=10},
+	damage_groups = {cracky=1, snappy=1, level=1},
+	on_unequip = function(player, index, stack)
+		rythium_light_users[player:get_player_name()] = nil
+	end,
 })
 
+local timer = 0
 minetest.register_globalstep(function(dtime)
-  rythium_light.timer=rythium_light.timer+dtime
-  if rythium_light.timer>1 then
-    rythium_light.timer=0
-    for i,ob in pairs(rythium_light.users) do
-      local pos=ob.player:get_pos()
-      pos.y=pos.y+1.5
-      local n=minetest.get_node(pos).name
-      local light=minetest.get_node_light(pos)
-      if light==nil then
-        rythium_light.users[i]=nil
-        return false
-      end
-      if ob.inside>10 or minetest.get_node_light(pos)>12 then
-        rythium_light.users[i]=nil
-      elseif n=="air" or n=="rythium:light" then
-        minetest.set_node(pos, {name="rythium:light"})
-      else
-        ob.inside=ob.inside+1
-      end
-    end
-  end
+	timer = timer + dtime
+	if timer > 1 then
+		timer = 0
+		for name, ob in pairs(rythium_light_users) do
+			local pos = ob.player:get_pos()
+			pos.y = pos.y + 1.5
+			local n = minetest.get_node(pos).name
+			local light = minetest.get_node_light(pos)
+			if not light then
+				rythium_light_users[name] = nil
+				return
+			end
+			if ob.inside > 10 or minetest.get_node_light(pos) > 12 then
+				rythium_light_users[name] = nil
+			elseif n == "air" then
+				minetest.set_node(pos, {name = "rythium:light"})
+			elseif n == "rythium:light" then
+				minetest.get_node_timer(pos):start(2)
+			else
+				ob.inside = ob.inside + 1
+			end
+		end
+	end
 end)
 

commit ce1298aea67fde96c464f4ea8b5483f5c8bdb291

Make rythium leaves generated again
Accidentally broken in 1f5177fea326f77212b25b425b154e2c16b9b4c7

tree.lua
@@ -67,6 +67,8 @@ local function add_trunk_and_leaves_rythium(data, a, pos, tree_cid, leaves_cid,
 			if data[vi] == c_air or data[vi] == c_ignore then
 				if random(1, 50) == 1 then
 					data[vi] = c_apple
+				else
+					data[vi] = leaves_cid
 				end
 			end
 			vi = vi + 1
@@ -85,6 +87,8 @@ local function add_trunk_and_leaves_rythium(data, a, pos, tree_cid, leaves_cid,
 			if data[vi] == c_air or data[vi] == c_ignore then
 				if random(1, 25) == 1 then
 					data[vi] = c_apple
+				else
+					data[vi] = leaves_cid
 				end
 			end
 		end